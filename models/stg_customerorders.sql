7c0d4939876b3e6c912c533a2cf9cf695a2cd9e5
Sharath Karthikeya <sharath.karthikeya@gmail.com>
2022-01-07 12:27:52 +0000

    base model build complete

---
+++ models/stg_customerorders.sql
@@ -0,0 +1,12 @@
+with customers_orders AS (
+  SELECT
+    customer_id,
+    MIN(order_date) AS first_order_date,
+    MAX(order_date) AS most_recent_order_date,
+    COUNT(order_id) AS number_of_orders
+  FROM
+    {{ ref('stg_orders') }}
+  GROUP BY
+     customer_id 
+)
+select * from customers_orders
---
+++ models/stg_orders.sql
@@ -0,0 +1,12 @@
+
+with orders AS (
+  SELECT
+    id AS order_id,
+    user_id AS customer_id,
+    order_date,
+    status
+  FROM `dbt-tutorial.jaffle_shop.orders`
+
+)
+
+select * from orders
